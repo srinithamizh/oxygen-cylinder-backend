dfe575c775c7c77670086d65987bcc734a306d09
Thamizharasan Srinivasan <srinithamizh@gmail.com>
2024-07-25 08:51:11 +0530

    Initial Commit: Set up project structure

---
+++ src/main/java/in/srinithamizh/oxygen_cylinder/OxygenCylinderApplication.java
@@ -0,0 +1,13 @@
+package in.srinithamizh.oxygen_cylinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxygenCylinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxygenCylinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/srinithamizh/oxygen_cylinder/OxygenCylinderApplicationTests.java
@@ -0,0 +1,13 @@
+package in.srinithamizh.oxygen_cylinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxygenCylinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
